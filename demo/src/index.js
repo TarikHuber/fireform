ff97a8cc93470b852f058d782b230bae117fce33
TarikHuber <huber.tarik@gmail.com>
2019-05-15 11:18:29 +0200

    Removed demo code

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,26 +1,4 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
-import { Provider } from 'react-redux';
-import { FirebaseProvider } from 'firekit';
-import configureStore from './store';
-import { Root } from './containers/Root';
-import { addLocalizationData } from './locales';
-import { firebaseApp } from './firebase';
+import React, { Component } from 'react'
+import { render } from 'react-dom'
 
-const store = configureStore();
-
-addLocalizationData();
-
-class Demo extends Component {
-  render() {
-    return <div>
-      <Provider store={store}>
-        <FirebaseProvider firebaseApp={firebaseApp}>
-          <Root />
-        </FirebaseProvider>
-      </Provider>
-    </div>
-  }
-}
-
-render(<Demo/>, document.querySelector('#demo'))
+render(<div />, document.querySelector('#demo'))
