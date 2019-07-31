c3cb9cf472936ec9299df033f27f96a8e0f1163a
Tarik Huber <tarik.huber@ics.local>
2019-07-31 12:01:08 +0200

    Refactored for official release

--- src/components/FireForm.js
+++ src/components/FireForm.js
@@ -1,47 +1,41 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
-import { initialize } from 'redux-form'
 import { connect } from 'react-redux'
+import { initialize } from 'redux-form'
+
+const clean = o => {
+  Object.keys(o).forEach(key => o[key] === undefined && delete o[key])
+  return o
+}
+
+const getCreateValues = (values, props) => {
+  const { handleCreateValues } = props
+
+  if (handleCreateValues !== undefined && handleCreateValues instanceof Function) {
+    return handleCreateValues(values)
+  }
+
+  return values
+}
+
+const getUpdateValues = (values, props) => {
+  const { handleUpdateValues } = props
+
+  if (handleUpdateValues !== undefined && handleUpdateValues instanceof Function) {
+    return handleUpdateValues(values)
+  }
+
+  return values
+}
 
 class FireForm extends Component {
-  constructor(props, context) {
-    super()
-    this.context = context
-    this.state = {
-      initialized: false
-    }
-  }
-
-  clean(obj) {
-    Object.keys(obj).forEach(key => obj[key] === undefined && delete obj[key])
-    return obj
-  }
-
-  getCreateValues = values => {
-    const { handleCreateValues } = this.props
-
-    if (handleCreateValues !== undefined && handleCreateValues instanceof Function) {
-      return handleCreateValues(values)
-    }
-
-    return values
-  }
-
-  getUpdateValues = (values, dispatch, props) => {
-    const { handleUpdateValues } = this.props
-
-    if (handleUpdateValues !== undefined && handleUpdateValues instanceof Function) {
-      return handleUpdateValues(values)
-    }
-
-    return values
-  }
+  state = { initialized: false }
 
   handleSubmit = values => {
     const { path, uid, firebaseApp, useFirestore } = this.props
 
     if (uid) {
-      const updateValues = this.getUpdateValues(this.clean(values))
+      const updateValues = getUpdateValues(clean(values), this.props)
       if (updateValues) {
         if (useFirestore) {
           firebaseApp
@@ -58,7 +52,7 @@ class FireForm extends Component {
         }
       }
     } else {
-      const createValues = this.getCreateValues(this.clean(values))
+      const createValues = getCreateValues(clean(values), this.props)
 
       if (createValues) {
         if (useFirestore) {
@@ -78,37 +72,7 @@ class FireForm extends Component {
     }
   }
 
-  handleDelete = () => {
-    const { onDelete, path, uid, firebaseApp, useFirestore } = this.props
-
-    if (uid) {
-      if (useFirestore) {
-        firebaseApp
-          .firestore()
-          .collection(path)
-          .doc(uid)
-          .delete()
-          .then(() => {
-            if (onDelete && onDelete instanceof Function) {
-              onDelete()
-            }
-          })
-      } else {
-        firebaseApp
-          .database()
-          .ref()
-          .child(`${path}${uid}`)
-          .remove()
-          .then(() => {
-            if (onDelete && onDelete instanceof Function) {
-              onDelete()
-            }
-          })
-      }
-    }
-  }
-
-  componentWillMount() {
+  componentDidMount() {
     const { path, uid, name, firebaseApp, initialize, useFirestore } = this.props
 
     if (uid) {
@@ -117,13 +81,18 @@ class FireForm extends Component {
           .firestore()
           .collection(path)
           .doc(uid)
-          .onSnapshot(doc => {
-            if (doc.exists) {
-              this.setState({ initialized: true }, () => {
-                initialize(name, doc.data(), true)
-              })
+          .onSnapshot(
+            doc => {
+              if (doc.exists) {
+                this.setState({ initialized: true }, () => {
+                  initialize(name, doc.data(), true)
+                })
+              }
+            },
+            err => {
+              console.log(`Encountered error: ${err}`)
             }
-          })
+          )
       } else {
         firebaseApp
           .database()
@@ -136,40 +105,6 @@ class FireForm extends Component {
       }
     } else {
       this.setState({ initialValues: {}, initialized: true })
-    }
-  }
-
-  componentWillReceiveProps = nextProps => {
-    const { uid, name, path, firebaseApp, initialize, useFirestore } = this.props
-    const { uid: nextUid } = nextProps
-
-    if (uid && uid !== nextUid) {
-      if (useFirestore) {
-        if (this.unsub) {
-          this.unsub()
-        }
-
-        this.unsub = firebaseApp
-          .firestore()
-          .collection(path)
-          .doc(nextUid)
-          .onSnapshot(doc => {
-            if (doc.exists) {
-              this.setState({ initialized: true }, () => {
-                initialize(name, doc.data(), true)
-              })
-            }
-          })
-      } else {
-        firebaseApp
-          .database()
-          .ref(`${path}${nextUid}`)
-          .on('value', snapshot => {
-            this.setState({ initialized: true }, () => {
-              initialize(name, snapshot.val(), true)
-            })
-          })
-      }
     }
   }
 
@@ -199,7 +134,7 @@ class FireForm extends Component {
 FireForm.propTypes = {
   path: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  useFirestore: PropTypes.bool,
+  useFirestore: PropTypes.any,
   firebaseApp: PropTypes.any.isRequired,
   uid: PropTypes.string,
   onDelete: PropTypes.func,
